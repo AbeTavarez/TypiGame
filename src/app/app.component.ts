8668fa14a53c1a14fad28cf38f92cf05762d33bb
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-28 21:13:46 -0500

    Getting user input and comparing it to the random text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
 import { Component, Input } from '@angular/core';
-import {lorem} from 'faker';
+import { lorem, random } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  randomText = lorem.sentence();
+  randomText: string = lorem.sentence();
+  enteredText: string = '';
+  msg: string = '';
 
-  handleInput(event: Event){
+  handleInput(event: Event) {
     const target = event.target as HTMLInputElement;
     const value = target.value;
     console.log(value);
-    
+    this.enteredText = value;
+  };
+
+  compare(randomLetter: string, enteredLetter: string){
+    if (!enteredLetter) return 'pending';
+
+    return randomLetter === enteredLetter ? 'correct': 'incorrect';
+
+  }
+
+  displayMsg(enteredText: string, randomText: string) {
+    if (enteredText === randomText) return 'You Win!';
+    else return '';
   }
 }
